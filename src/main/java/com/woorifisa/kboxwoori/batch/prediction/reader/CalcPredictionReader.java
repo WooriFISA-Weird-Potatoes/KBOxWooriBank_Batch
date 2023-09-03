3349b21c5d2335282ca9d27a382b9d704cb4564a
Dain Lee <dain9934@gmail.com>
2023-09-03 23:00:48 +0900

    feat: 포인트 정산 ItemReader 구현

---
+++ src/main/java/com/woorifisa/kboxwoori/batch/prediction/reader/CalcPredictionReader.java
@@ -0,0 +1,36 @@
+package com.woorifisa.kboxwoori.batch.prediction.reader;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+public class CalcPredictionReader implements ItemReader<Map.Entry<Object, Object>> {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    private static final int COUNT = 100;
+    private static final String PREDICTION_KEY = "prediction:";
+
+    private Cursor<Map.Entry<Object, Object>> cursor;
+
+    @PostConstruct
+    public void init() {
+        ScanOptions options = ScanOptions.scanOptions().count(COUNT).build();
+        this.cursor = redisTemplate.opsForHash().scan(PREDICTION_KEY + LocalDate.now(), options);
+    }
+
+    @Override
+    public Map.Entry<Object, Object> read() {
+        log.info("Item Reader read()");
+        return cursor.hasNext() ? cursor.next() : null;
+    }
+}
