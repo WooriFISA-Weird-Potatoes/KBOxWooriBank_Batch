93a3e3f95f5a6a9fa2e569df2d932c4cea11529c
Dain Lee <dain9934@gmail.com>
2023-09-03 23:01:57 +0900

    feat: 포인트 내역 갱신 ItemWriter 구현

---
+++ src/main/java/com/woorifisa/kboxwoori/batch/prediction/writer/UpdatePointHistoryWriter.java
@@ -0,0 +1,45 @@
+package com.woorifisa.kboxwoori.batch.prediction.writer;
+
+import com.woorifisa.kboxwoori.batch.prediction.UserPrediction;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+public class UpdatePointHistoryWriter implements ItemWriter<UserPrediction> {
+
+    private final DataSource dataSource;
+
+    private static final String UPDATE_POINT_HISTORY_SQL = "INSERT INTO point_history (user_id, status_code, point, created_at) VALUES (?, ?, ?, ?)";
+    private static final String POINT_HISTORY_STATUS = "SAVE";
+
+    @Override
+    public void write(List<? extends UserPrediction> items) throws Exception {
+        JdbcBatchItemWriter<UserPrediction> writer = new JdbcBatchItemWriterBuilder<UserPrediction>()
+                .dataSource(dataSource)
+                .sql(UPDATE_POINT_HISTORY_SQL)
+                .itemPreparedStatementSetter(new ItemPreparedStatementSetter<UserPrediction>() {
+                    @Override
+                    public void setValues(UserPrediction item, PreparedStatement ps) throws SQLException {
+                        ps.setLong(1, item.getId());
+                        ps.setString(2, POINT_HISTORY_STATUS);
+                        ps.setInt(3, item.getExtraPoints());
+                        ps.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
+                    }
+                })
+                .build();
+
+        writer.write(items);
+    }
+}
