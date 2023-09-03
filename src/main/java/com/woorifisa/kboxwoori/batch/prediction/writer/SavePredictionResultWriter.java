b9fbbda738caf366cc9a932bdd8ab3f8fe655c9c
Dain Lee <dain9934@gmail.com>
2023-09-03 23:02:13 +0900

    feat: 승부예측 결과 갱신 ItemWriter 구현

---
+++ src/main/java/com/woorifisa/kboxwoori/batch/prediction/writer/SavePredictionResultWriter.java
@@ -0,0 +1,43 @@
+package com.woorifisa.kboxwoori.batch.prediction.writer;
+
+import com.woorifisa.kboxwoori.batch.prediction.UserPrediction;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+
+import javax.sql.DataSource;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+public class SavePredictionResultWriter implements ItemWriter<UserPrediction> {
+
+    private final DataSource dataSource;
+
+    private static final String SAVE_PREDICTION_RESULT_SQL = "INSERT INTO prediction_history (user_id, is_correct, created_at) VALUES (?, ?, ?)";
+
+    @Override
+    public void write(List<? extends UserPrediction> items) throws Exception {
+        JdbcBatchItemWriter<UserPrediction> writer = new JdbcBatchItemWriterBuilder<UserPrediction>()
+                .dataSource(dataSource)
+                .sql(SAVE_PREDICTION_RESULT_SQL)
+                .itemPreparedStatementSetter(new ItemPreparedStatementSetter<UserPrediction>() {
+                    @Override
+                    public void setValues(UserPrediction item, PreparedStatement ps) throws SQLException {
+                        ps.setLong(1, item.getId());
+                        ps.setBoolean(2, item.getExtraPoints() != 0);
+                        ps.setDate(3, Date.valueOf(LocalDate.now()));
+                    }
+                })
+                .build();
+
+        writer.write(items);
+    }
+}
