af2dbeb7ac61dbba0ab30e2b4d0c17e0d412d194
Dain Lee <dain9934@gmail.com>
2023-09-04 00:56:19 +0900

    feat: 승부예측 참여자 수 itemReader 구현

---
+++ src/main/java/com/woorifisa/kboxwoori/batch/admin/reader/SavePredictionParticipantCountReader.java
@@ -0,0 +1,29 @@
+package com.woorifisa.kboxwoori.batch.admin.reader;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.LocalDate;
+
+@Slf4j
+@RequiredArgsConstructor
+public class SavePredictionParticipantCountReader implements ItemReader<Long> {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    private static final String PREDICTION_KEY = "prediction:";
+
+    private boolean isRead = false;
+
+    @Override
+    public Long read() {
+        if (!isRead) {
+            isRead = true;
+            return redisTemplate.opsForHash().size(PREDICTION_KEY + LocalDate.now());
+        }
+
+        return null;
+    }
+}
